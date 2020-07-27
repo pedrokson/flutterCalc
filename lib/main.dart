183922b8bccc9a5698d15355aaa538217259826f
Mateus Antonio <mateuscfc8@outlook.com>
2020-07-27 23:51:28 +0000

    Trocar inteiro para double e adicionar tratamento inicial de zeros

--- lib/main.dart
+++ lib/main.dart
@@ -30,47 +30,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _primeiroNumero = 0;
-  String _operacoes = "Cx+-=/";
-  String _operacaoSelecionada = "";
+  String _operacoes = 'Cx+-=/';
+  String _operacaoSelecionada = '';
 
-  String _resultadoParcial = "";
-  int _total = 0;
+  double _primeiroNumero = 0;
+  double _total = 0;
+
+  String _resultadoParcial = '';
+  String _resultadoFinal = '0';
 
   /// Callback passada para os botões que retorna o botão pressionado
   /// no atributo [nome]
   void pressionarBotao(String nome) {
     if (!_operacoes.contains(nome)) {
-      setState(() {
-        if (_total != 0) {
-          /// Se já houver número digitado, concatena o novo número a ele
-          _total = int.parse(_total.toString() + nome);
-        } else {
-          _total = int.parse(nome);
-        }
-      });
+      _total != 0
+          ? _atualizarTotal(double.parse(_tratarZeros(_total) + nome))
+          : _atualizarTotal(double.parse(nome));
     } else {
       if (nome == 'C') {
+        _atualizarTotal();
         setState(() {
-          _total = 0;
           _resultadoParcial = "";
           _operacaoSelecionada = "";
         });
       } else {
         if (nome == '=') {
+          _atualizarTotal(_calcularResultadoOperacao(
+              _primeiroNumero, _total, _operacaoSelecionada));
           setState(() {
-            _total = _calcularResultadoOperacao(
-                _primeiroNumero, _total, _operacaoSelecionada);
-            _resultadoParcial = "";
-            _operacaoSelecionada = "";
+            _resultadoParcial = '';
+            _operacaoSelecionada = '';
           });
         } else {
           _primeiroNumero = _total;
           _operacaoSelecionada = nome;
+          _atualizarTotal();
           setState(() {
-            _total = 0;
             _resultadoParcial =
-                _primeiroNumero.toString() + _operacaoSelecionada;
+                _tratarZeros(_primeiroNumero) + _operacaoSelecionada;
           });
         }
       }
@@ -79,7 +76,7 @@ class _HomePageState extends State<HomePage> {
 
   /// Realiza o cálculo da [operacao] passada tomando [num1] como primeiro
   /// termo e [num2] como segundo termo
-  int _calcularResultadoOperacao(int num1, int num2, String operacao) {
+  double _calcularResultadoOperacao(double num1, double num2, String operacao) {
     if (operacao == '+') {
       return num1 + num2;
     } else if (operacao == '-') {
@@ -87,8 +84,26 @@ class _HomePageState extends State<HomePage> {
     } else if (operacao == 'x') {
       return num1 * num2;
     } else {
-      return num1 ~/ num2;
+      return num1 / num2;
     }
+  }
+
+  String _tratarZeros(double n) {
+    String valorStr = n.toStringAsFixed(6);
+    String antesPonto = valorStr.split('.')[0];
+    String depoisPonto = valorStr.split('.')[1];
+    if (depoisPonto.replaceAll('0', '').length == 0) {
+      return antesPonto;
+    } else {
+      return valorStr;
+    }
+  }
+
+  void _atualizarTotal([double n = 0]) {
+    setState(() {
+      _total = n;
+      _resultadoFinal = _tratarZeros(n);
+    });
   }
 
   @override
@@ -102,7 +117,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            CalcScreen(_resultadoParcial, _total.toString()),
+            CalcScreen(_resultadoParcial, _resultadoFinal),
             Divider(color: Colors.black),
             ButtonRow(['7', '8', '9', '/'], this.pressionarBotao),
             ButtonRow(['4', '5', '6', 'x'], this.pressionarBotao),
